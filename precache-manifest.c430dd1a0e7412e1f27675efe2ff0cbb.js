954a0a9eb600e52563285f10792091ae90b25a20
DhruvJain1122 <amritkumarjain321@gmail.com>
2022-02-18 23:28:17 +0530

    Updates

---
+++ precache-manifest.c430dd1a0e7412e1f27675efe2ff0cbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354f22ffdc4b9e8c3ae343178dc8348b",
+    "url": "/Prnts-Interface/index.html"
+  },
+  {
+    "revision": "5505479297e5004da667",
+    "url": "/Prnts-Interface/static/css/main.0ea357e1.chunk.css"
+  },
+  {
+    "revision": "b34dc70ef6c5cd34d1f7",
+    "url": "/Prnts-Interface/static/js/2.865d5c9d.chunk.js"
+  },
+  {
+    "revision": "5505479297e5004da667",
+    "url": "/Prnts-Interface/static/js/main.6c332e17.chunk.js"
+  },
+  {
+    "revision": "a163588c42d780469587",
+    "url": "/Prnts-Interface/static/js/runtime-main.db776c53.js"
+  },
+  {
+    "revision": "a28c43509436a175cf9b8f8c1bbd64ff",
+    "url": "/Prnts-Interface/static/media/Intro-image2.a28c4350.jpg"
+  },
+  {
+    "revision": "2784f9549a856e89fde582e6d04a2668",
+    "url": "/Prnts-Interface/static/media/coinbase-wallet-logo.2784f954.svg"
+  },
+  {
+    "revision": "e1648d049502ca199f3090090d193c47",
+    "url": "/Prnts-Interface/static/media/default-profile.e1648d04.jpg"
+  },
+  {
+    "revision": "3cb96389bc27ea4aac9c752bed68ddd8",
+    "url": "/Prnts-Interface/static/media/matic-token-icon.3cb96389.webp"
+  },
+  {
+    "revision": "4b56c01cd6b4c2e6dcb364eb4cf38f6f",
+    "url": "/Prnts-Interface/static/media/prnts-logo.4b56c01c.png"
+  },
+  {
+    "revision": "3d48bf6e3fbf88b0e66ef441d3b4100a",
+    "url": "/Prnts-Interface/static/media/profile.3d48bf6e.png"
+  }
+]);
